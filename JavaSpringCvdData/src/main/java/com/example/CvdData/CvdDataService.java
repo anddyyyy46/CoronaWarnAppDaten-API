a8a513b56e84bb005f7b77554cd084fb38b3d954
Andreas Rautenberg <andyrau99@gmail.com>
2023-07-12 18:01:30 +0200

    added docs

--- JavaSpringCvdData/src/main/java/com/example/CvdData/CvdDataService.java
+++ JavaSpringCvdData/src/main/java/com/example/CvdData/CvdDataService.java
@@ -12,9 +12,9 @@ public class CvdDataService {
     CvdDataRepository cvdDataRepository;
 
     /**
-     * Manages the requested number of a specific range
-     * @param date was das macht...
-     * @return was das macht...
+     * Manages the output depending on the input date/range
+     * @param date Either just one character, a year, a month and a year, a specific day or a range from two specific day
+     * @return List of the data of every day in the requested range
      */
     public List<CvdDataModel> getData(String date) {
         switch (date.length()){
@@ -28,16 +28,15 @@ public class CvdDataService {
                 return cvdDataRepository.findByDate(date);
             case 21:                                                //Number each day in a specific range
                 return cvdDataRepository.findByRange(date.split("-")[0], date.split("-")[1]);
-
             default:
                 return null;
         }
     }
 
     /**
-     * Manages the requested averages
-     * @param date
-     * @return
+     * Manages the output of the average numbers of  a given date/range
+     * @param date Either one letter or a year
+     * @return List of the average infections per day in a given range
      */
     public List<Average> getDataAverages(String date){
         switch (date.length()){
@@ -51,9 +50,9 @@ public class CvdDataService {
     }
 
     /**
-     * @return List of the average object with the average numbers in a year and
-     * the related year and a month which is always null because all given getters/setters
-     * will be used when converting java object to JSON
+     * @return List of the average object, which contains the average infections per day in a year and
+     * the related year. The month attribute is always null because all given getters/setters
+     * will be used, when converting java object to JSON, although they are not necessary.
      */
     public List<Average> getAvrgYears(){
         double averages = 0;
@@ -72,16 +71,15 @@ public class CvdDataService {
     }
 
     /**
-     * Returns a list of the average object with the average numbers in a month and the related month and year
-     * @param allDaysinaYear
-     * @return
+     * @param allDaysinaYear List of the data from every day in the given year
+     * @return List of the average object, which contains the average infections per day in a month and the related month and year
      */
     public List<Average> getAvrgMonths(List<CvdDataModel> allDaysinaYear){
         List<Average> averages = new ArrayList<>();
         String month = "";
         int numbers = 0;
         int days = 0;
-        final String year = allDaysinaYear.get(0).getDate().substring(0,4);
+        String year = allDaysinaYear.get(0).getDate().substring(0,4);
         for(int i = 0; i<allDaysinaYear.size(); i++){
             month = allDaysinaYear.get(i).getDate().substring(5, 7);
             while(month.equals(allDaysinaYear.get(i).getDate().substring(5, 7))){
@@ -96,13 +94,10 @@ public class CvdDataService {
     }
 
     /**
-     * Returns a List of all the years in the dataset
-     * @return
+     * @return List of all the years in the dataset
      */
     public List<String> getAllYears(){
-        System.out.println("test");
         List<CvdDataModel> allData = cvdDataRepository.getAll();
-        System.out.println(allData.get(0).getDate());
         String currentYear = allData.get(0).getDate().substring(0,4);
         List<String> allYears = new ArrayList<>();
         for(int i =1; i<allData.size();i++){
